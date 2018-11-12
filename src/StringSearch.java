f485fea727f5732947e824fd3b89af73bfbb667b
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-11-12 11:44:20 +0300

    Update src/StringSearch.java

--- src/StringSearch.java
+++ src/StringSearch.java
@@ -2,14 +2,21 @@ public class StringSearch {
     static public void main (String[] args)
     {
         String[] example_text = {"Java string", "XML string", "html string",
-                                 "cpp string", "python string", "NOT string",
+                                 "cpp string", "python string", "NOT fail",
                                 };
         int counter1 = 0, counter2 = 0, counter3 = 0;
 
         for (int i = 0; i < example_text.length; i++)
         {
             System.out.print(example_text[i].substring(0, 4) + ' ');
+            if (example_text[i].endsWith("string")) { counter1 ++; }
+            if (example_text[i].startsWith("Java")) { counter2 ++; }
+            if (example_text[i].indexOf("fail") == -1) {counter3 ++; }
         }
+
+        System.out.println("\ncounter1 = " + Integer.toString(counter1));
+        System.out.println("counter2 = " + Integer.toString(counter2));
+        System.out.println("counter3 = " + Integer.toString(counter3));
 
     }
 }
