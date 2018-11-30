c2eb6b1c902f4b88073af5da16f77927f0f96b82
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-11-30 16:52:11 +0300

    exception

---
+++ src/beat_box/MusicTest1.java
@@ -0,0 +1,23 @@
+package beat_box;
+
+import javax.sound.midi.*;
+
+public class MusicTest1 {
+
+    public void play(){
+        try{
+            Sequencer sequencer = MidiSystem.getSequencer();
+            System.out.println("Мы получили синтезатор!!!");
+        } catch (Exception ex) {
+            System.out.println("Не удалось получить синтезатор. ");
+            ex.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args){
+        MusicTest1 mt = new MusicTest1();
+        mt.play();
+
+    }
+}
