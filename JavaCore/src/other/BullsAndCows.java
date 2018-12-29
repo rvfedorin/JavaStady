63c0656c38577b098e68e35c4f433670a778a5d2
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-12-29 17:06:30 +0300

    ended BullsAndCows

--- JavaCore/src/other/BullsAndCows.java
+++ JavaCore/src/other/BullsAndCows.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class BullsAndCows {
     private ArrayList<Integer> guessNumber;
     private static final int LENGTH_NUMBER = 3;
-    private int cows = 0;
-    private int bulls = 0;
+    private static final int NUMBER_OF_TRY = 10;
+    private static final String QUIT = "q";
 
     public static void main(String[] args) {
         BullsAndCows game = new BullsAndCows();
@@ -40,22 +40,73 @@ public class BullsAndCows {
     } // close startGame()
 
     private void handleGame() {
+        boolean gameRun = true;
+        Scanner cons = new Scanner(System.in);
+        String userGuess;
+        String result;
+        char[] userNumber;
+        int tryNum = 0;
+
         System.out.println(guessNumber);
         System.out.print("You think it is: ");
-        Scanner cons = new Scanner(System.in);
-        String userGuess = cons.nextLine();
+        while (gameRun) {
+            tryNum += 1;
 
-        char[] userNumber = userGuess.toCharArray();
+            userGuess = cons.nextLine();
 
-        for (int i = 0; i < LENGTH_NUMBER; i++) {
-            int dig = Character.getNumericValue(userNumber[i]);
-            if (dig == guessNumber.get(i)) {
-                bulls +=1;
-            } else if (guessNumber.contains(dig)) {
-                cows +=1;
+            if (userGuess.equals(QUIT)) {
+                System.out.println("Bye.");
+                break;
+            }
+
+            userNumber = userGuess.toCharArray();
+            if (userNumber.length < LENGTH_NUMBER) {
+                System.out.println("For quit enter [q]");
+                System.out.print("You need enter 3 number: ");
+                continue;
+            }
+
+            result = checkNumber(userNumber);
+
+            if (result.equals("WIN")) {
+                System.out.println("You win!");
+                gameRun = false;
+            } else if (tryNum < NUMBER_OF_TRY){
+                System.out.println(result);
+                System.out.print("Try again: ");
+            } else {
+                System.out.println("You lost.");
+                gameRun = false;
             }
         }
-        System.out.println("Bulls: " + bulls + " Cows: " + cows);
-    }
+
+    } // close handleGame()
+
+    private String checkNumber(char[] uGess) {
+        int cows = 0;
+        int bulls = 0;
+        ArrayList<Integer> tempNum = (ArrayList<Integer>) guessNumber.clone();
+
+        for (int i = 0; i < LENGTH_NUMBER; i++) {
+            int dig = Character.getNumericValue(uGess[i]);
+
+            if (dig > 10) {
+                return "You need enter number or [q] to quit";
+            }
+
+            if (dig == guessNumber.get(i)) {
+                bulls +=1;
+                tempNum.remove((Integer) dig);
+            } else if (tempNum.contains(dig)) {
+                cows +=1;
+                tempNum.remove((Integer) dig);
+            }
+        }
+        if (bulls == LENGTH_NUMBER) {
+            return ("WIN");
+        } else {
+            return ("Bulls: " + bulls + " Cows: " + cows);
+        }
+    }  // close String checkNumber(char[] uGess)
 
 }
