d09bc54e2ab33344170c8db35dd99acde0e35469
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2019-01-20 20:48:36 +0300

    Update HelpJava.java

--- Java8Beginner/src/part3/HelpJava.java
+++ Java8Beginner/src/part3/HelpJava.java
@@ -4,29 +4,59 @@ import java.io.IOException;
 
 public class HelpJava {
     public static void main (String[] args){
-        char inp;
+        char inp, unnecessary;
+        boolean exit;
         try {
-            System.out.println("Справка.");
-            System.out.println("1. if");
-            System.out.println("2. switch");
-            System.out.print("Выбирете: ");
-            inp = (char) System.in.read();
+            do {
+                exit = false;
+                for (int i = 0; i < 20; i++) {
+                    System.out.print("=");
+                }
+                System.out.println();
 
-            if (inp == '1') {
-                System.out.println("if - условное ветвление.");
-                System.out.println("if (условие) оператор");
-                System.out.println("else оператор");
-            } else if ( inp == '2') {
-                System.out.println("switch - выбор ветвления");
-                System.out.println("switch (выражение) {");
-                System.out.println("\tcase константа: ");
-                System.out.println("\tпоследовательность операторов");
-                System.out.println("\tbreak;");
-                System.out.println("\t// ...");
-                System.out.println("}");
-            } else {
-                System.out.println("Раздел не найден.");
-            }
+                System.out.println("Справка.");
+                System.out.println("Для выхода введите q");
+                System.out.println("\t1. \tif");
+                System.out.println("\t2. \tswitch");
+                System.out.println("\t3. \tfor");
+                System.out.println("\t4. \twhile");
+                System.out.print("Выбирете: ");
+
+                inp = (char) System.in.read();
+                do {
+                    unnecessary = (char) System.in.read();
+                } while (unnecessary != '\n');
+
+                if (inp == '1') {
+                    System.out.println("if - условное ветвление.");
+                    System.out.println("if (условие) оператор");
+                    System.out.println("else оператор");
+                } else if (inp == '2') {
+                    System.out.println("switch - выбор ветвления");
+                    System.out.println("switch (выражение) {");
+                    System.out.println("\tcase константа: ");
+                    System.out.println("\tпоследовательность операторов");
+                    System.out.println("\tbreak;");
+                    System.out.println("\t// ...");
+                    System.out.println("}");
+                } else if (inp == '3') {
+                    System.out.println("for - цикл");
+                    System.out.println("for (инициализация; условие; итерация) {");
+                    System.out.println("\tпоследовательность операторов");
+                    System.out.println("}");
+                } else if (inp == '4') {
+                    System.out.println("while - цикл");
+                    System.out.println("while (условие) {");
+                    System.out.println("\tпоследовательность операторов");
+                    System.out.println("}");
+                } else if (inp == 'q') {
+                    System.out.println("Выход.");
+                } else {
+                    System.out.println("Раздел не найден.");
+                    exit = true;
+                }
+            } while(exit);
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
