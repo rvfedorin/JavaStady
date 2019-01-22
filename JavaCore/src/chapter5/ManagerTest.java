b95f4af90ca4ebcdd82a2f890e7a2525dd101fae
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2019-01-22 11:51:18 +0300

    changed ManagerTest to ArrayList

--- JavaCore/src/chapter5/ManagerTest.java
+++ JavaCore/src/chapter5/ManagerTest.java
@@ -1,21 +1,23 @@
 package chapter5;
 
+import java.util.*;
+
 public class ManagerTest {
     public static void main(String[] args) {
         Manager boss = new Manager("Boss", 80000, 22, 4, 2019);
         boss.setBonus(5000);
 
-        Employee[] staff = new Employee[3];
+        ArrayList<Employee> staff = new ArrayList<Employee>();
 
         System.out.println(boss.getClass());
 
-        staff[0] = boss;
-        staff[1] = new Employee("Tom", 40000, 12, 8, 2019);
-        staff[2] = new Employee("Nancy", 40000, 12, 8, 2019);
+        staff.add(boss);
+        staff.add(new Employee("Tom", 40000, 12, 8, 2019));
+        staff.add(new Employee("Nancy", 40000, 12, 8, 2019));
 
         for(Employee e: staff) {
             System.out.println(e.getSalary());
-            System.out.println("instanceof: " + (e instanceof Employee));
+            System.out.println("instanceof: " + (e instanceof Manager));
             System.out.println("Class: " + e.getClass());
             System.out.println(e);
             System.out.println("============================");
