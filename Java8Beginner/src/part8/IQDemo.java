5d8b9a27423ed01c8297d73a3a10f9709f8d4b0c
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2019-02-12 16:00:12 +0300

    added circular queue

--- Java8Beginner/src/part8/IQDemo.java
+++ Java8Beginner/src/part8/IQDemo.java
@@ -3,14 +3,37 @@ package part8;
 public class IQDemo {
     public static void main(String[] args) {
         FixedQueue fq = new FixedQueue(2);
+        System.out.println("It is the first get " + fq.get());
         fq.put('a');
         fq.put('d');
         fq.put('e');
+        System.out.println("It is the END added ");
         System.out.println(fq.get());
         System.out.println(fq.get());
         System.out.println(fq.get());
 
+        printLine();
+
+        CircularQueue cq = new CircularQueue(3);
+        System.out.println(cq.get());
+        cq.put('a');
+        cq.put('d');
+        cq.put('e');
+        System.out.println(cq.get());
+        System.out.println(cq.get());
+        System.out.println(cq.get());
+
+        printLine();
+
     } // main()
+
+    private static void printLine() {
+        for (int i = 0; i < 40; i++) {
+            System.out.print("=");
+        }
+        System.out.println();
+    }
+
 } // IQDemo
 
 class FixedQueue implements ICharQ {
@@ -40,4 +63,47 @@ class FixedQueue implements ICharQ {
         }
         return (char) 0;
     }
-}
+} // class FixedQueue
+
+class CircularQueue implements ICharQ {
+    private char[] queue;
+    private int putloc, getloc;
+    private boolean notEmpty;
+
+    CircularQueue(int size) {
+        queue = new char[size];
+        putloc = getloc = 0;
+        notEmpty = false;
+    }
+
+    @Override
+    public void put(char ch) {
+        if (putloc < queue.length) {
+            queue[putloc] = ch;
+            putloc += 1;
+            if (notEmpty) getloc += 1;
+            notEmpty = true;
+        } else {
+            putloc = getloc = 0;
+            queue[putloc] = ch;
+        }
+    }
+
+    @Override
+    public char get() {
+        char res;
+        if (getloc > 0 && notEmpty) {
+            putloc -= 1;
+            res = queue[getloc];
+            getloc -= 1;
+        } else if(getloc == 0 && notEmpty) {
+            putloc = 0;
+            res = queue[getloc];
+            getloc = queue.length-1;
+        } else {
+            System.out.print("Empty");
+            res = (char) 0;
+        }
+        return res;
+    }
+} // class CircularQueue
