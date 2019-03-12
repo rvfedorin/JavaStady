a4c4eafbc94d00b946011acfc368adf734ea98ba
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2019-03-12 12:55:29 +0300

    Update test.java

--- JavaCore/src/other/test.java
+++ JavaCore/src/other/test.java
@@ -4,17 +4,28 @@ public class test {
     public static void main(String[] args) {
         String first = "pass";
         String second = "key";
-
 //        System.out.println(encode(first, second));
 
-        int it = 24;
+        int it = 55;
         int sub = 0xfffffff8;
         int res = it & sub;
-        System.out.println(it << 1);
-        System.out.println(Integer.toBinaryString(sub));
-        System.out.println(Integer.toBinaryString(it));
-        System.out.println(Integer.toBinaryString(res));
-        System.out.println(res);
+        System.out.println(it << 8);
+//        System.out.println(0 & 0x0ff);
+//        System.out.println(Integer.toBinaryString(sub));
+//        System.out.println(Integer.toBinaryString(it));
+//        System.out.println(Integer.toBinaryString(res));
+//        System.out.println(res);
+
+        int nStrLen = first.length();
+        int nI;
+        int nPos = 0;
+        for (nI = 0; nI < nStrLen; nI++)
+        {
+            char cActChar = first.charAt(nI);
+            System.out.println((byte) ((cActChar >> 8) & 0x0ff));
+            System.out.println((byte) (cActChar & 0x0ff));
+        }
+
 
     }  // close main
 
