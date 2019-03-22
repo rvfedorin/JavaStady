91af77db98955c4f16c7124a4ebbdbd3a5981a00
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2019-03-22 11:02:19 +0300

    Update SimpleFrameTest.java
    added drawing of image

--- JavaCore/src/chapter10/SimpleFrameTest.java
+++ JavaCore/src/chapter10/SimpleFrameTest.java
@@ -2,13 +2,16 @@ package chapter10;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
 
 public class SimpleFrameTest {
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             SimpleFrame frame = new SimpleFrame("Моё окно.");
+
             frame.add(new TextLabel());
-            frame.pack();
+//            frame.pack();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setVisible(true);
         });
@@ -26,7 +29,7 @@ class SimpleFrame extends JFrame {
         Dimension dimension = kit.getScreenSize();
         int scrWidth = dimension.width;
         int scrHeight = dimension.height;
-//        setSize(scrWidth/4, (int) (scrHeight/2.85));
+        setSize(scrWidth/4, (int) (scrHeight/2.85));
     }
 
     SimpleFrame(String title) {
@@ -41,11 +44,32 @@ class TextLabel extends JComponent {
 
     @Override
     public void paintComponent(Graphics g) {
-        g.drawString("Строка в окне!!", X, Y);
+        String message = "Строка в окне!!";
+
+        Graphics2D g2 = (Graphics2D) g;
+        Font sansBold14 = new Font("SansSerif", Font.BOLD, 14);
+        g2.setFont(sansBold14);
+        FontRenderContext context = g2.getFontRenderContext();
+        Rectangle2D bounds = sansBold14.getStringBounds(message, context);
+
+        g2.drawRect((int) bounds.getX() + X,
+                    (int) bounds.getY() + Y,
+                       (int) bounds.getWidth(),
+                       (int) bounds.getHeight());
+        g2.drawString(message, X, Y);
     }
 
     @Override
     public Dimension getPreferredSize() {
+
         return new Dimension(X*5, Y*2);
+    }
+}  // class TextLabel
+
+class MyImage extends JComponent {
+    @Override
+    public void paintComponent(Graphics g) {
+        Image img = new ImageIcon("ico.jpg").getImage();
+        g.drawImage(img, 150, 150, null);
     }
 }
