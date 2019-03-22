f3d5f7c394f640f249e74e1acdb3b2f2f16a7eb3
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2019-03-22 15:03:23 +0300

    Update ButtonFrame.java
    added the ability to switch the frame view

--- JavaCore/src/chapter11/ButtonFrame.java
+++ JavaCore/src/chapter11/ButtonFrame.java
@@ -15,7 +15,7 @@ public class ButtonFrame extends JFrame {
 
         JButton yellowButton = new JButton("Yellow");
         JButton blueButton = new JButton("Blue");
-        JButton redButton = new JButton("Red");
+        JButton redButton = new JButton("<html><hr>Red<hr></html>");
 
         buttonPane = new JPanel();
 
@@ -23,15 +23,35 @@ public class ButtonFrame extends JFrame {
         buttonPane.add(blueButton);
         buttonPane.add(redButton);
 
+        UIManager.LookAndFeelInfo[] lookInfo = UIManager.getInstalledLookAndFeels();
+
+        for (UIManager.LookAndFeelInfo lookAndFeel: lookInfo){
+            String name = lookAndFeel.getName();
+            String className = lookAndFeel.getClassName();
+
+            JButton but = new JButton(name);
+            buttonPane.add(but);
+            but.addActionListener(event -> {
+                try {
+                    UIManager.setLookAndFeel(className);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                SwingUtilities.updateComponentTreeUI(this);
+            });
+        }
+
         this.add(buttonPane);
 
         ColorAction yAct = new ColorAction(Color.YELLOW);
         ColorAction bAct = new ColorAction(Color.BLUE);
-        ColorAction rAct = new ColorAction(Color.RED);
+//        ColorAction rAct = new ColorAction(Color.RED);
 
         yellowButton.addActionListener(yAct);
         blueButton.addActionListener(bAct);
-        redButton.addActionListener(rAct);
+        redButton.addActionListener(event -> {
+            buttonPane.setBackground(Color.RED);
+        });
 
     } // constr ()
 
