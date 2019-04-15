88f2c155459df77e5fc5a8355793868f5310bb87
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2019-04-15 17:38:35 +0300

    parsed path and client

--- JavaCore/src/MyWork/WebIntranet/ConnectToIntranet.java
+++ JavaCore/src/MyWork/WebIntranet/ConnectToIntranet.java
@@ -7,12 +7,14 @@ import org.jsoup.Jsoup;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static MyWork.Tools.ConvertRussian.convertToRus;
 import static MyWork.Config.*;
 
 public class ConnectToIntranet {
-    private static final String PASS = "passpass$";
+    private static final String PASS = "!purumpumpum!";
 
     private static final String url = "https://intranet.ptl.ru/AutorizUser.php";
     private static final String connectionURL = "https://intranet.ptl.ru/connection/";
@@ -24,12 +26,17 @@ public class ConnectToIntranet {
         loginCookies.put("ActiveRegName", "Ростов-на-Дону");
 
         getBody(connectionURL, loginCookies); // list of connection in region ActiveReg
-        String customer = findCustomer(loginCookies, "RND-Panda");
+
+        // return Map with keys: status, where, row, mnemokod, ipCl, vlan, address, ipCon, tel, description;
+        Map<String, String> customer = findCustomer(loginCookies, "RND-Panda");
+
         String customerConnect = findCustomerConnect(loginCookies, "1"); // id - number client in previous getBody()
 
-        System.out.println(convertToRus(customer));
+        if(customer != null && !customer.get("status").contains("Error")) {
+            System.out.println(customer);
+        }
         System.out.println(LINE);
-        System.out.println(customerConnect);
+        System.out.println(ParsePath(convertToRus(customerConnect)));
     }
 
     public static Map<String, String> connectAndGetCookie(String url){
@@ -69,9 +76,10 @@ public class ConnectToIntranet {
         return result;
     } // ** getBody(utl, cookie)
 
-    public static String findCustomer(Map<String, String> cookie, String data) {
+    public static Map<String, String> findCustomer(Map<String, String> cookie, String data) {
+        // return Map with keys: status, where, row, mnemokod, ipCl, vlan, address, ipCon, tel, description;
         Connection.Response response;
-        String result;
+        Map<String, String> result = new HashMap<>();
 
         cookie.put("ActivePage", "1");
         cookie.put("LimitElems", "10");
@@ -85,16 +93,44 @@ public class ConnectToIntranet {
                     .data("data", data)
                     .execute();
         } catch (IOException ex) {
-            return "Error. " + ex.toString();
+            result.put("status", "Error. " + ex.toString());
+            return result;
         }
 
         if (response == null || response.body() == null)
-            result = "Error findCustomer()";
-        else
-            result = getMapResponse(response.body()).getOrDefault(
-                    "result",
-                    "Error from getMapResponse");
+            result.put("status", "Error findCustomer()");
+        else {
+            String notFormatted = response.body();
 
+            if(notFormatted != null && !notFormatted.contains("Error")) {
+                notFormatted = convertToRus(notFormatted);
+
+                Pattern patternHead = Pattern.compile("status\":" +
+                        "(\\d{1,3}).*" +
+                        "(\\d{1,5}).*showMore'>" +
+                        "([\\w\\d-]*)(.*)");
+
+                Matcher res = patternHead.matcher(notFormatted);
+                if (res.find()) {
+                    result.put("status", res.group(1));
+                    result.put("row", res.group(2));
+                    result.put("mnemokod", res.group(3));
+
+                    String[] tileS = res.group(4).split("\\\\n");
+                    result.put("ipCl", getTextInTD(tileS[1]));
+                    result.put("vlan", getTextInTD(tileS[2]));
+                    result.put("address", getTextInTD(tileS[3]));
+                    result.put("ipCon", getTextInTD(tileS[4]));
+                    result.put("tel", getTextInTD(tileS[5]));
+                    result.put("description", getTextInTD(tileS[6]));
+                } else {
+                    result.put("status", "Error Pattern no found.)");
+                } // ** if find
+            } else {
+                result.put("status", "Error body()");
+            }// ** if fine body
+        } // if get response
+//        System.out.println(result);
         return result;
     }
 
@@ -124,25 +160,67 @@ public class ConnectToIntranet {
         return result;
     }
 
-    private static Map<String, String> getMapResponse(String toParse) {
-        // keys = status, result;
-        toParse = toParse.replaceAll(",\"", "xXxX\"");
-        toParse = toParse.trim();
-        toParse = toParse.substring(1, toParse.length()-1);
+//    private static Map<String, String> getMapResponse(String toParse) {
+//        // keys = status, result;
+//        toParse = toParse.replaceAll(",\"", "xXxX\"");
+//        toParse = toParse.trim();
+//        toParse = toParse.substring(1, toParse.length()-1);
+//
+//        String[] sAr = toParse.split("xXxX");
+//        Map<String, String> result = new HashMap<>();
+//
+//        for (String ss: sAr) {
+//            String[] temp = ss.split("\":");
+//
+//            if(temp.length == 2)
+//                result.put(temp[0].replaceAll("\"", "").trim(), temp[1]);
+//            else
+//                result.put("Error", ss);
+//        }
+//
+//        return result;
+//    }
 
-        String[] sAr = toParse.split("xXxX");
-        Map<String, String> result = new HashMap<>();
-
-        for (String ss: sAr) {
-            String[] temp = ss.split("\":");
-
-            if(temp.length == 2)
-                result.put(temp[0].replaceAll("\"", "").trim(), temp[1]);
-            else
-                result.put("Error", ss);
-        }
-
-        return result;
+    private static String getTextInTD(String s) {
+        Pattern inTd = Pattern.compile("<td>(.*)<\\\\/td>");
+        Matcher matcher = inTd.matcher(s);
+        if (matcher.find())
+            return matcher.group(1);
+        else
+            return "";
     }
+
+    private static String ParsePath(String s) {
+        String leftPort = "";
+        String rightPort = "";
+        String ip = "";
+
+        StringBuilder path = new StringBuilder();
+
+        for(String temp: s.split("auto")) {
+            leftPort = "";
+            rightPort = "";
+            ip = "";
+//            System.out.println(temp);
+
+            String[] row = temp.split("<br>");
+            Pattern port = Pattern.compile("\\[.*]");
+            Pattern ipP = Pattern.compile("\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}");
+
+            if (row.length > 1) {
+                Matcher leftPortM = port.matcher(row[0]);
+                Matcher ipMatch = ipP.matcher(row[1]);
+                Matcher rightPortM = port.matcher(row[1]);
+
+                if(leftPortM.find()) leftPort = leftPortM.group();
+                if(ipMatch.find()) ip = ipMatch.group();
+                if(rightPortM.find()) rightPort = rightPortM.group();
+
+                path.append(leftPort + " " + ip + " " + rightPort + " <=> ");
+            } // ** if(row.length > 1)
+        } // ** for
+
+        return path.toString();
+    } // ** ParsePath(String s)
 
 } // ** class
--- JavaCore/src/other/test.java
+++ JavaCore/src/other/test.java
@@ -1,38 +1,88 @@
 package other;
 
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class test {
     public static void main(String[] args) {
-//№	Имя	IP-адрес(a)	VLAN	Адрес установки	Привязанные устройства	Ном. телефона	Описание
-
-        Pattern pattern = Pattern.compile("\\{\"status\":(\\d*),\"result\":\" <tr class='resSearch'> <td colspan='9' class='text-center'>(\\w*)");
-        String s1 = " {\"status\":200,\"result\":\" <tr class='resSearch'> <td colspan='9' class='text-center'>Результат найден в разделе 'Устройства по региону'<\\/td> <\\/tr> <tr class='resSearch'  >\\n                <th scope='row'>1<\\/th>\\n                <td class='showMore'>RND-Panda<\\/td>\\n                <td>88.86.80.23<\\/td>\\n                <td>vlan321<\\/td>\\n                <td>ул. Володарского 2-я, 76<\\/td>\\n                <td>172.17.196.78 - rwr<\\/td>                                                                             \\n                <td><\\/td> \\n                <td><\\/td>                        \\n                <\\/tr>\"}";
-        String s2 =  "{\"status\":200,\"map\":\"<div class='tree'> <ul> <li><div data-toggle=\\\"tooltip\\\" data-placement=\\\"auto\\\" data-html=\\\"true\\\" title=\\\" <table> <tbody>\\n                        <tr> <th scope='row'>\\u041c\\u043e\\u0434\\u0435\\u043b\\u044c:<\\/th> <td>Cisco ASR1001 <\\/td> <\\/tr> <\\/tbody> <\\/table>\\\" >\\n                         \\n                        <a  href='\\/devices\\/?show=60&reg=6' target=\\\"_blank\\\">Core Cisco<\\/a> <br>95.80.127.253\\n                        <p class=\\\"ports\\\">[0] <\\/p><\\/div> <ul> <li><div data-toggle=\\\"tooltip\\\" data-placement=\\\"auto\\\" data-html=\\\"true\\\" title=\\\" <table> <tbody>\\n                        <tr> <th scope='row'>\\u041c\\u043e\\u0434\\u0435\\u043b\\u044c:<\\/th> <td>D-Link DGS-3120-24TC<\\/td> <\\/tr> <\\/tbody> <\\/table>\\\" >\\n                        <p class=\\\"ports\\\">[1] <\\/p>\\n                        <a  href='\\/devices\\/?show=61&reg=6' target=\\\"_blank\\\">Core Switch<\\/a> <br>172.17.199.254\\n                        <p class=\\\"ports\\\">[22] <\\/p><\\/div> <ul> <li><div data-toggle=\\\"tooltip\\\" data-placement=\\\"auto\\\" data-html=\\\"true\\\" title=\\\" <table> <tbody>\\n                        <tr> <th scope='row'>\\u041c\\u043e\\u0434\\u0435\\u043b\\u044c:<\\/th> <td>D-Link DES-3200-10 Fast Ethernet Switch<\\/td> <\\/tr> <\\/tbody> <\\/table>\\\" >\\n                        <p class=\\\"ports\\\">[10] <\\/p>\\n                        <a  href='\\/devices\\/?show=62&reg=6' target=\\\"_blank\\\">Base1 ORTPC Sector switch<\\/a> <br>172.17.199.250\\n                        <p class=\\\"ports\\\">[2] <\\/p><\\/div> <ul> <li><div data-toggle=\\\"tooltip\\\" data-placement=\\\"auto\\\" title=\\\"\\u041c\\u043e\\u0434\\u0435\\u043b\\u044c:SkyMAN R5000-Omxb\\\" >\\n                <p class=\\\"ports\\\"><\\/p>\\n                <a href='\\/devices\\/?show=64&reg=6' target=\\\"_blank\\\">Base1 ORTPC RVR SectorA<\\/a> <br>172.17.196.2<p class=\\\"ports\\\">[0] <\\/p><\\/div> <ul> <li> <div data-toggle=\\\"tooltip\\\" data-placement=\\\"auto\\\" data-html=\\\"true\\\" title=\\\" <table> <tbody>\\n                        <tr> <th scope='row'>\\u041c\\u043e\\u0434\\u0435\\u043b\\u044c:<\\/th> <td>SkyMAN R5000-Sm\\/5.300.2x63.2x21<\\/td> <\\/tr>                        \\n                        <tr> <th scope='row'>VLAN:<\\/th>   <td><\\/td> <\\/tr> <\\/tbody> <\\/table>\\\" >\\n                        <p class=\\\"ports\\\">[0] <\\/p>\\n                        <a  href='\\/devices\\/?show=93&reg=6'   target=\\\"_blank\\\">RND-Panda<\\/a> <br>172.17.196.78 \\n                        <p class=\\\"ports\\\" data-toggle=\\\"tooltip\\\" data-placement=\\\"bottom\\\" data-html=\\\"true\\\" title=\\\"<span class='oi oi-person'> <\\/span>RND-Panda\\\" >[0] <\\/p> <\\/div> <\\/li>  <\\/ul> <\\/li> <\\/ul> <\\/li> <\\/ul> <\\/li> <\\/ul> <\\/li> <\\/ul> <\\/div>\"}";
-
-        s1 = s1.replaceAll(",\"", "xXxX\"");
-        s1 = s1.trim();
-        s1 = s1.substring(1, s1.length()-1);
-
-        String[] sAr = s1.split("xXxX");
         Map<String, String> result = new HashMap<>();
 
-        for (String ss: sAr) {
-            String[] temp = ss.split("\":");
+        String s1 = "{\"status\":200,\"result\":\"<tr class='resSearch'  >\\n                <th scope='row'>1<\\/th>\\n                <td class='showMore'>RND-Eldakoyug<\\/td>\\n                <td>88.86.80.109<\\/td>\\n                <td>vlan103<\\/td>\\n                <td>\\u0443\\u043b. 1-\\u0430\\u044f \\u041b\\u0443\\u0433\\u043e\\u0432\\u0430\\u044f, \\u0434\\u043e\\u043c \\u2116 17, \\u043a\\u043e\\u0440\\u043f. \\\"\\u0411\\\"<\\/td>\\n                <td>172.18.40.90 - rwr<\\/td>                                                                             \\n                <td><\\/td> \\n                <td><\\/td>                        \\n                <\\/tr>\"}";
 
-            if(temp.length == 2)
-                result.put(temp[0].replaceAll("\"", "").trim(), temp[1]);
-            else
-                result.put("Error", ss);
-        }
-        result.forEach((k, v) -> {
-            System.out.println("Key = " + k);
-            System.out.println("Value = " + v);
-        });
+        String row, mnemo, ipCl, vlan, address, ipCon, tel, descr;
+
+//        ParsePath(s1);
+
+        Pattern patternHead = Pattern.compile("status\":" +
+                "(\\d{1,3}).*" +
+                "(\\d{1,5}).*showMore'>" +
+                "([\\w\\d-]*)(.*)");
+
+        Matcher res = patternHead.matcher(s1);
+        if (res.find()) {
+            result.put("status", res.group(1));
+            result.put("row", res.group(2));
+            result.put("mnemokod", res.group(3));
+
+            String[] tileS = res.group(4).split("\\\\n");
+            result.put("ipCl", getTextInTD(tileS[1]));
+            result.put("vlan", getTextInTD(tileS[2]));
+            result.put("address", getTextInTD(tileS[3]));
+            result.put("ipCon", getTextInTD(tileS[4]));
+            result.put("tel", getTextInTD(tileS[5]));
+            result.put("description", getTextInTD(tileS[6]));
+        } else {
+            result.put("status", "Error Pattern no found.)");
+        } // ** if find
+
+        System.out.println(result);
+
     } // ** main()
+
+    private static String ParsePath(String s) {
+        String leftPort = "";
+        String rightPort = "";
+        String ip = "";
+
+        StringBuilder path = new StringBuilder();
+
+        for(String temp: s.split("auto")) {
+            leftPort = "";
+            rightPort = "";
+            ip = "";
+            System.out.println(temp);
+
+            String[] row = temp.split("<br>");
+            Pattern port = Pattern.compile("\\[.*]");
+            Pattern ipP = Pattern.compile("\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}");
+
+            if (row.length > 1) {
+                Matcher leftPortM = port.matcher(row[0]);
+                Matcher ipMatch = ipP.matcher(row[1]);
+                Matcher rightPortM = port.matcher(row[1]);
+
+                if(leftPortM.find()) leftPort = leftPortM.group();
+                if(ipMatch.find()) ip = ipMatch.group();
+                if(rightPortM.find()) rightPort = rightPortM.group();
+
+                path.append(leftPort + " " + ip + " " + rightPort + " <=> ");
+
+            } // ** if(row.length > 1)
+        } // ** for
+        System.out.println(path.toString());
+        return path.toString();
+    } // ** ParsePath(String s)
+
+    private static String getTextInTD(String s) {
+        Pattern inTd = Pattern.compile("<td>(.*)<\\\\/td>");
+        Matcher matcher = inTd.matcher(s);
+        if (matcher.find())
+            return matcher.group(1);
+        else
+            return "";
+    }
 }
 
 // repeat text /////////////////////////////////////////////////////////////////////////////////
