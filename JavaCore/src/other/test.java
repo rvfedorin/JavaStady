ba9c4430fb0f33c050f9050873ee442709b0395d
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2019-09-04 15:23:46 +0300

    created MB config

--- JavaCore/src/other/test.java
+++ JavaCore/src/other/test.java
@@ -1,25 +1,10 @@
 package other;
 
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-
 public class test {
     public static void main(String[] args) {
-        String fileName = "./test.txt";
 
-        try {
-            Files.write(
-                    Paths.get(fileName),
-                    "\ntest test1".getBytes(),
-                    Files.exists(Paths.get(fileName)) ? StandardOpenOption.APPEND : StandardOpenOption.CREATE_NEW);
-        } catch (IOException ex) {
-            System.out.println("Error opening file");
-        }
     }
-
 } // ** class test
 
 
