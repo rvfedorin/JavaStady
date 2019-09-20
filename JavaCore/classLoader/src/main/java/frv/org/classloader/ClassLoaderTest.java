242e93f0c9948ab9062f1a85e0586579aeba71b8
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2019-09-20 11:18:52 +0300

    endWithIt

--- JavaCore/classLoader/src/main/java/frv/org/classloader/ClassLoaderTest.java
+++ JavaCore/classLoader/src/main/java/frv/org/classloader/ClassLoaderTest.java
@@ -6,7 +6,6 @@ package frv.org.classloader;
 import java.awt.EventQueue;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
-import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
@@ -52,7 +51,7 @@ class ClassLoaderFrame extends JFrame {
         add(new JLabel("Key "), new GBC(0, 1).anchor("east"));
         add(nameField, new GBC(1, 0).anchor("west").weight(100, 0));
         add(keyField, new GBC(1, 1).anchor("west").weight(100, 0));
-        
+
         JButton loadButton = new JButton("Load");
         add(loadButton, new GBC(0, 2).gridWH(2, 1));
         loadButton.addActionListener((ActionEvent e) -> {
@@ -61,54 +60,55 @@ class ClassLoaderFrame extends JFrame {
 
         pack();
     } // ** constructor
-    
+
     public void runClass(String name, String key) {
         try {
             ClassLoader loader = new CryptoClassLoader(Integer.parseInt(key));
             Class<?> c = loader.loadClass(name);
             Method m = c.getMethod("main", String[].class);
-            m.invoke(null, (Object) new String[] {});
+            m.invoke(null, (Object) new String[]{});
         } catch (Throwable ex) {
             Logger.getLogger(ClassLoaderFrame.class.getName()).log(Level.SEVERE, null, ex);
             JOptionPane.showMessageDialog(this, ex);
         }
-        
+
     } // ** runClass()
 
 } // ** ClassLoaderFrame
 
 class CryptoClassLoader extends ClassLoader {
+
     private int key;
 
     public CryptoClassLoader(int key) {
         this.key = key;
     }
-    
+
     @Override
     public Class<?> findClass(String name) throws ClassNotFoundException {
         try {
             byte[] classBytes = null;
             classBytes = loadClassBytes(name);
             Class<?> cl = defineClass(name, classBytes, 0, classBytes.length);
-            if(cl == null) throw new ClassNotFoundException(name);
+            if (cl == null) {
+                throw new ClassNotFoundException(name);
+            }
             return cl;
         } catch (IOException ex) {
             ex.printStackTrace();
             throw new ClassNotFoundException(name);
         }
     } // ** findClass()
-    
+
     public byte[] loadClassBytes(String name) throws IOException {
         String cname = name.replace('.', '/') + ".crypt";
         cname = "resources/" + cname;
-        cname = this.getClass().getResource(cname).getPath();
-        cname = cname.substring(cname.indexOf(":")+1, cname.length());
+
         byte[] bytes = Files.readAllBytes(Paths.get(cname));
-        for(int i = 0; i < bytes.length; i++) {
-            byte temp = bytes[i];
-            bytes[i] = (byte)(bytes[i] - key);
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) (bytes[i] - key);
         }
         return bytes;
     }
-    
+
 } // ** CryptoClassLoader
