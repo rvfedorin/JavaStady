9a20ceb3e6cb50d40626342003ad9198f82e04a1
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2019-10-11 05:42:49 +0300

    demo ligging aspect

---
+++ NetBeansProjects/spring-demo-aop/src/main/java/rv/fedorin/spring/demoaop/DemoConfig.java
@@ -0,0 +1,21 @@
+package rv.fedorin.spring.demoaop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/*
+ * Proprietary software.
+ * 
+ */
+
+/**
+ *
+ * @author R. V. Fedorin
+ */
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("rv.fedorin.spring.demoaop")
+public class DemoConfig {
+    
+}
